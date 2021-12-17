2192602de6a014b8899f665966352f358306a29d
Yogendra <you@example.com>
2021-12-17 13:50:20 +0530

    ADDED : Get Request Method To Show Hello + Name From Bridgelabz

--- src/main/java/com/example/demo/controller/RestController.java
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/hello")
@@ -8,5 +10,10 @@ public class RestController {
     @RequestMapping(value = {"", "/", "home"})
     public String sayHello() {
         return "Hello From Bridgelabz..";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value= "name")String name){
+        return "Hello "+ name +" From Bridgelabz";
     }
 }
